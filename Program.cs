89f7e0c14504050229c7688a4e66e99f3c20ae1b
injakakhulu <st10464050@rcconnect.edu.za>
2026-05-29 20:55:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using POEp1.Forms;
+using System;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Windows.Forms;
+using POEp1.Forms;
+
+using System;
+using System.Windows.Forms;
+using POEp1.Forms;
+
+namespace POEp1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // Force the WinForms Application explicitly
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+            // Fully qualify the form
+            System.Windows.Forms.Application.Run(new POEp1.Forms.MainForm());
+        }
+    }
+}
